91706d2ff371e8eb69b17da82c28ca5e3c1344a7
Quadrosh <quadrosh@gmail.com>
2017-10-20 21:32:52 +0300

    favicon

--- yapp/frontend/views/layouts/master.php
+++ yapp/frontend/views/layouts/master.php
@@ -20,6 +20,10 @@ AppAsset::register($this);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Tinos" rel="stylesheet">
+    <link rel="apple-touch-icon" href="/img/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="256x256" href="/img/favicon/apple-touch-icon-256x256.png">
     <?= Html::csrfMetaTags() ?>
 
     <title> <?= Yii::$app->view->params['meta']['title'] ?> </title>
