e877cd5d3d6f12c4ba74356f3855c8d9a8534d66
Quadrosh <quadrosh@gmail.com>
2017-11-07 17:56:29 +0300

    sms ready

--- yapp/frontend/controllers/SendController.php
+++ yapp/frontend/controllers/SendController.php
@@ -7,6 +7,7 @@ use common\models\Master;
 use Yii;
 use yii\filters\AccessControl;
 use yii\helpers\Url;
+use yii\httpclient\Client;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 
@@ -76,6 +77,24 @@ class SendController extends Controller
 
         if ($feedback->save()) {
 
+            if ($master['order_phone']!=null) {
+                $client = new Client();
+                $response = $client->createRequest()
+                    ->setMethod('post')
+                    ->setUrl('https://sms.ru/sms/send')
+                    ->setData([
+                        'api_id' => '4940EAEB-EAD2-89D5-E5CE-F61C7FC262EE',
+                        'to' => '79853461615, '.$master['order_phone'],
+                        'text'=> 'Psihotera'.$master['id'].'- заявка с сайта, имя - '.$feedback['name'].', телефон - '.$feedback['phone'],
+                    ])
+                    ->send();
+                if ($response->isOk) {
+                    Yii::$app->session->setFlash('success','сообщение отправлено');
+                } else {
+                    Yii::$app->session->setFlash('error','что-то пошло не так');
+                }
+
+            }
             if ($feedback->sendEmail( 'PSIHOTERA.RU: Заявка',$master['email'])) {
                 Yii::$app->session->setFlash('success', 'Ваша заявка отправлена. <br> Мы свяжемся с Вами в ближайшее время.');
                 return $this->redirect(Url::previous());
